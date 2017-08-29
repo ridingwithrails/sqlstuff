65d3fcc605d888b639a6659e88e55deb59057887
Anis Tayebali <anis.tayebali@hyfn.com>
2017-08-29 13:16:26 -0700

    Added a join with a subquery

--- sql_stuff.sql
+++ sql_stuff.sql
@@ -370,3 +370,21 @@ from students s join grades g
 on s.marks between g.min_mark and g.max_mark
 order by grade desc, s.name asc,
 case when grade <= 8 then s.marks end asc;
+
+
+/*
+Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+*/
+
+select h.hacker_id, h.name from (
+  -- Must not use aliases in count it does not work
+  -- alias challenge_count can be accessed outside of sub query
+select s.hacker_id as hid, count(*) as challenge_count from submissions s
+    join challenges c on s.challenge_id = c.challenge_id
+    join difficulty d on c.difficulty_level = d.difficulty_level
+    where d.score = s.score
+    group by hid
+    having challenge_count > 1)
+as t
+join hackers h on hid = h.hacker_id
+order by challenge_count desc, hid asc
